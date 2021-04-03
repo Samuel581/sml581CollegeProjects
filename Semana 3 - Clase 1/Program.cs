9a075126dbc5677f574a76422143b3ca950ad932
user <user@pop-os.localdomain>
2021-04-03 16:24:55 -0600

    Add: Week 3 - Class 1

---
+++ Semana 3 - Clase 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Semana_3___Clase_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
